52999ca33e56db1dbbc92f914cb999ce6308d2bc
Lesetja <kmalapane155@gmail.com>
2025-08-23 08:40:20 +0200

    added html skeleton for conditions input

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 console.log("Hello, world");
 
 const weather_crossing_api_key = "HVLE4KL3U5FZXW3LW55V46T8V";
-let my_url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london?key=${weather_crossing_api_key}`;
 
-function useApi() {
-  return new Promise((resolve) => {
-    if (my_url != null) {
-      const response = fetch(my_url, { mode: "cors" });
-      resolve(response);
-    }
-  }).then(function(message) {return message.json()})
-  // .then(data => console.log(data));
+async function useApi(location) {
+  let my_url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${weather_crossing_api_key}`;
+  
+  const data = await fetch(my_url, {mode: "cors"})
+  const weatherInfo = await data.json();
+
+  return weatherInfo;
+  // return new Promise((resolve) => {
+  //   if (my_url != null) {
+  //     const response = fetch(my_url, { mode: "cors" });
+  //     resolve(response);
+  //   }
+  // }).then(function(message) {return message.json()})
+  // // .then(data => console.log(data));
 }
 
 function printCurrentWeatherConditions(jsonMessage) {
@@ -37,5 +42,5 @@ console.log(`${jsonMessage.address}`)
 }
 
 
-let hello = useApi();
+let hello = useApi("Johannesburg");
 console.log(hello.then(message => printCurrentWeatherConditions(message)))
